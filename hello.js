712b4f94f5cc59bf6933be09ae61907fa44f204f
ravali90 <ravali90@yahoo.com>
2016-01-08 23:13:11 -0800

    changed port

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var app=express();
-//var port = process.env.PORT || 1337;
+var port = process.env.PORT || 1337;
 app.use(express.static('public'));
 app.get('/',function(req,res){
 
@@ -8,6 +8,6 @@ app.get('/',function(req,res){
 res.sendFile(__dirname + '/public/index.html');
 
 });
-var server=app.listen(3000,function(){
+var server=app.listen(port,function(){
 	console.log("server started listening");
 });
